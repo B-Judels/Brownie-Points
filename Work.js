e55d90ce86cd8731239631a48a670663cc8d483f
Bernard <judelsbernard2@gmail.com>
2024-10-30 10:38:35 +0200

    adding some functionality

--- Work.js
+++ Work.js
@@ -1,6 +1,8 @@
 let count = 0;
 
-
-
-
 document.getElementById("counter").innerHTML = `${count}`;
+
+document.getElementById("btn").addEventListener("click", function() {
+    count++;
+    document.getElementById("counter").innerHTML = `${count}`;
+});

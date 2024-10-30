e9f9b1657758f0e23c41029da3b1f6aed557fc97
Bernard <judelsbernard2@gmail.com>
2024-10-30 10:42:01 +0200

    some more playing around

--- Work.js
+++ Work.js
@@ -2,6 +2,7 @@ let count = 0;
 
 document.getElementById("counter").innerHTML = `${count}`;
 
+
 document.getElementById("btn").addEventListener("click", function() {
     count++;
     document.getElementById("counter").innerHTML = `${count}`;

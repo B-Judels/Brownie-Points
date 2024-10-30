35c4ce4ea8b2cb9deb58561aeda670a585b8e09e
Bernard <judelsbernard2@gmail.com>
2024-10-30 12:49:53 +0200

    some more playing arround

--- Work.js
+++ Work.js
@@ -1,9 +1,24 @@
 let count = 0;
 
-document.getElementById("counter").innerHTML = `${count}`;
+document.getElementById("counter").innerHTML = `Points: ${count}`;
 
 
-document.getElementById("btn").addEventListener("click", function() {
+document.getElementById("btn1").addEventListener("click", function() {
     count++;
-    document.getElementById("counter").innerHTML = `${count}`;
+    document.getElementById("counter").innerHTML = `Points: ${count}`;
+});
+
+document.getElementById("btn10").addEventListener("click", function() {
+    count += 10;
+    document.getElementById("counter").innerHTML = `Points: ${count}`;
+});
+
+document.getElementById("btn50").addEventListener("click", function() {
+    count += 50;
+    document.getElementById("counter").innerHTML = `Points: ${count}`;
+});
+
+document.getElementById("btn100").addEventListener("click", function() {
+    count += 100;
+    document.getElementById("counter").innerHTML = `Points: ${count}`;
 });

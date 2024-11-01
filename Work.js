9d665af679ba3ec927dd0dce59673cbfafbee336
Bernard <judelsbernard2@gmail.com>
2024-11-01 21:28:07 +0200

    made it mor euser friendly.

--- Work.js
+++ Work.js
@@ -23,22 +23,52 @@ document.getElementById("btn100").addEventListener("click", function() {
     document.getElementById("counter").innerHTML = `Points: ${count}`;
 });
 
+
+
+
+
 document.getElementById("redeem1").addEventListener("click", function() {
-    count -= 500;
-    document.getElementById("counter").innerHTML = `Points: ${count}`;
+    if(count >= 500)
+    {
+        count -= 500;
+        document.getElementById("counter").innerHTML = `Points: ${count}`;
+    }else{
+        alert("You do not have enough brownie points to redeem :( " + ` [Need ${500 - count}]`)
+    }
+    
 });
 
 document.getElementById("redeem2").addEventListener("click", function() {
-    count -= 1000;
-    document.getElementById("counter").innerHTML = `Points: ${count}`;
+    if(count >= 1000)
+    {
+        count -= 1000;
+        document.getElementById("counter").innerHTML = `Points: ${count}`;
+    }else{
+        alert("You do not have enough brownie points to redeem :( " + ` [Need ${1000 - count}]`)
+    }
+    
 });
 
 document.getElementById("redeem3").addEventListener("click", function() {
-    count -= 1500;
-    document.getElementById("counter").innerHTML = `Points: ${count}`;
+    if(count >= 1500)
+    {
+        count -= 1500;
+        document.getElementById("counter").innerHTML = `Points: ${count}`;
+    }else{
+        alert("You do not have enough brownie points to redeem :( " + ` [Need ${1500 - count}]`)
+
+    }
+    
 });
 
 document.getElementById("redeem4").addEventListener("click", function() {
-    count -= 2000;
-    document.getElementById("counter").innerHTML = `Points: ${count}`;
+    if(count >= 2000)
+    {
+        count -= 2000;
+        document.getElementById("counter").innerHTML = `Points: ${count}`;
+    }else{
+        alert("You do not have enough brownie points to redeem :( " + ` [Need ${2000 - count}]`)
+        
+    }
+    
 });
